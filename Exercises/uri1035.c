92adff50372f682328fa00ceeafe5ab8353bff98
ewertonfelipee <53940331+ewertonfelipee@users.noreply.github.com>
2021-08-05 23:27:55 -0300

    Create uri1035.c

---
+++ Exercises/uri1035.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int SumCD = c + d, SumAB = a + b;
+    if((b > c) && (d > a) && (SumCD > SumAB) && (c && d) > 0 && a%2==0){
+        printf("Valores aceitos\n");
+    }
+    else {printf("Valores nao aceitos\n");}
+    return 0;
+}
