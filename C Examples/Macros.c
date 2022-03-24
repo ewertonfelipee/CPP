05e161d848dcd5b5fdfd943cb7bf28efd677144f
Ewerton Felipe Silva Marinho <ewertonfmarinho@protonmail.ch>
2022-03-24 15:30:55 -0300

    Macros studies

--- C Examples/Macros.c
+++ C Examples/Macros.c
@@ -8,15 +8,21 @@
 
 #define DEBUG
 #define VAR(x) printf("The variable "#x" has the value: %d\n", x);
+#define HELLO(x) printf("Hello, "#x "\n"); // #x is substituted by Ewerton in the main function
 
 int main(int argc, char **argv)
 {
-    int count = 100;
+#if 1
+    HELLO(Ewerton);
+    int x, count = 100;
     double area = AREA_CIRCULAR(5.0);
     printf("Area: %.3f\n", area);
     printf("Max: %d\n", MAX(4, 9));
+#endif
 #ifdef DEBUG
-    printf("%s\n%d\n", __TIME__, __LINE__);
+    printf("%d\n", x);
+    printf("%s\n%d\n%s\n%s\n%d\n", __TIME__, __LINE__, __DATE__, __FILE__, __STDC__);
+    // __STDC__ the value 1 if the compiler accepts standard C 
     VAR(count);
 #endif
 // The macros are resolved in the compilation time
