1c877cbeff9ea50725979459e19b3d0249ad23a5
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-05-31 21:43:38 -0300

    Ponteiro para Struct card

--- Structs/Struct1.c
+++ Structs/Struct1.c
@@ -10,6 +10,7 @@ int main()
     struct card cardAge = {17, "McLovin"};
     struct card *cardName = &cardAge;
 
-    printf("%d\n", cardName -> Age);
-    printf("%s\n", cardName -> name);
+    printf("A idade eh: %d e o nome eh: %s\n", cardName -> Age, cardName -> name);
+
+    return 0;
 }
